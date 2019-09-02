ba9c9240ea80a9e0c0e82be3dd1b0f9016ac1274
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-02 14:58:14 -0500

    Update variables.c

--- variables/1_variables/variables.c
+++ variables/1_variables/variables.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float a,b,c;
+int a1, b1;
 int main()
 {
-	int entero=32;
-	short int entero_corto=32;
-	long int entero_largo=32L;
-	unsigned int entero_sin_signo=32U;
-	float simple=12.8F;
-	double doble=12.8;
-	long double doble_extendido=12.8L;
-	float notacion_cientifica=4.5e-4;
-	char caracter='A';
-	char cadena[10]="Lenguaje C";
-	
-	printf("int:%d\n", entero); 
-	printf("Short int:%hd\n", entero_corto); 
-	printf("Long int:%ld\n", entero_largo); 
-	printf("Unsigned int:%u\n", entero_sin_signo); 
-	printf("Float:%f\n", simple); 
-	printf("Float NCientifica:%f\n", notacion_cientifica); 
-	printf("Double:%f\n", doble); 
-	printf("Double Double:%LF\n", doble_extendido); 
-	printf("Char:%c\n", caracter); 
-	printf("String:%s\n", cadena); 
-// fin
+	system("clear");
+	printf("Dame el primer nÃºmero\n");
+	scanf("%f",&a);
+	a1 = a;
+	printf("Dame el segundo nÃºmero\n");
+	scanf("%f",&b);
+	b1 = b;
+
+	printf("Suma\n");
+		c=a+b; // Suma o adiciÃ³n
+		printf("\t%f + %f = %f\n",a,b,c);
+	printf("Resta\n");
+		c=a-b; // Resta o diferencia
+		printf("\t%f - %f = %f\n",a,b,c);
+	printf("MultiplicaciÃ³n\n");
+		c=a*b; // MultiplicaciÃ³n o producto
+		printf("\t%f * %f = %f\n",a,b,c);
+	printf("DivisiÃ³n\n");
+		c=a/b; // DivisiÃ³n o cociente tipo real
+		printf("\t%f / %f = %f\n",a,b,c);
+	printf("MÃ³dulo\n");
+		c=a1%b1; // Resto o mÃ³dulo
+		printf("\tEl mÃ³dulo de %d/%d es %f\n",a1,b1,c);
+	printf("RaÃ­z cuadrada\n");
+		c=sqrt(a); // RaÃ­z cuadrada de el primer numero
+		printf("\tLa RaÃ­z cuadrada de %f es %f\n",b,c);
+			printf("RaÃ­z cuadrada\n");
+		c=sqrt(b); // RaÃ­z cuadrada de el segundo numero
+		printf("\tLa RaÃ­z cuadrada de %f es %f\n",a,c);
+	printf("Potencia\n");
+		c = pow(a, b); //Potencia
+	printf("\tEl nÃºmero %f elevado a la potencia %f en %f\n",a,b,c);
+	printf("resultado del reto viral agosto");
+		c=a/b*(b+b); 
+    printf("\t%f / %f * (%f + %f)= %f\n",a,b,b,b,c);
+    
+    
+		
 return 0;
 }
