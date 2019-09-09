2186776e8a61dbdef5826ccd404cc44a649dbd86
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-09 14:15:08 -0500

    Update scan.c

--- variables/6_read/scan.c
+++ variables/6_read/scan.c
@@ -17,3 +17,37 @@ int main()
  printf("Alumno : %d %0.3f %s\n",num_cuenta,promedio,nombre);
  return 0;
 } // Fin de programa
+
+
+#include <stdio.h> // Archivo de cabecera para subrutinas de e/s
+#include <stdlib.h> // Archivo de cabecera para subrutinas de utilidades
+
+
+int num_cuenta;	
+
+float promedio;
+char nombre[20];
+float cal1; 
+float cal2; 
+float cal3; 
+float cal4; 
+
+int main() 
+{ // Inicio
+ printf("Ingrese numero de cuenta:");
+ scanf("%d",&num_cuenta);
+ printf("Ingrese promedio:");
+ scanf("%f",&promedio);
+ printf("Ingrese nombre:");
+ scanf("%s",nombre);
+ printf("Ingrese primera calificacion:");
+ scanf("%f",cal1);
+ printf("Ingrese segunda calificacion:");
+ scanf("%f",cal2);
+ printf("Ingrese tercera calificacion:");
+ scanf("%f",cal3
+ );
+ // Imprime el registro
+ printf("Alumno : %d %f %s %f %f %f\n",num_cuenta,promedio,nombre, cal1, cal2, cal3);
+ return 0;
+} // Fin d
