562fcc2bc74f14b94442ffaf98743ee1bb32d614
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-12 13:49:41 -0500

    Update if2.c

--- control/if2.c
+++ control/if2.c
@@ -23,3 +23,33 @@ int main()
 	}
   return 0;
 } 	
+
+#include <stdio.h>
+int main()
+{
+	int edad;
+	printf("Introduce tu edad: ");
+	scanf("%d",&edad);
+
+	if (edad >17 && edad <35)
+	{
+		 printf("Tienes %d anios, eres mayor de edad",edad);
+	}
+	else if (edad < 18)
+	{
+		 printf("Tienes %d anios, eres menor de edad",edad);
+	}
+	else if (edad > 35 && edad <110)
+	{
+		 printf("Tienes %d anios, eres chavo-ruco",edad);
+	}
+	else if (edad > 110)
+	{
+		 printf("Tienes mas de %d anios, no eres humano",edad);
+  return 0;
+} 	
+	else
+	{
+	printf("Fin de programa\n");
+	}
+} 	
