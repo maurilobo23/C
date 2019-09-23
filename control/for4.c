af738bb2c367a350d5d674071a3c6e01d18aa32e
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-23 14:18:37 -0500

    Update for4.c

--- control/for4.c
+++ control/for4.c
@@ -43,3 +43,29 @@ int main(void)
   getchar();
   return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <conio.h>
+//#include <curses.h>
+
+int main(void)
+{
+  int x,y,z,cont;
+  z=1;
+  for (x=0;x<=5;x++){
+  	for (y=0;y<=5;y++){
+     		 printf("[%d]",z);
+		 z++;
+		    if (x>y)
+		    { 
+		        z=0;
+		    }
+  	        printf("%d\t",z);
+  	}
+  	
+     	printf("\n");
+  }
+  getchar();
+  return 0;
+}
