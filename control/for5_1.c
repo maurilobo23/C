3a22b06ada02607cb0f98c33e39eab1323c2ac53
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-23 14:46:06 -0500

    Update for5_1.c

--- control/for5_1.c
+++ control/for5_1.c
@@ -15,3 +15,34 @@ int main(void)
   getchar();
   return 0;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+  int x, y, z, cont, stop;
+  x=0;
+  y=1;
+  printf("dame un numero\n");
+  scanf("%d",&stop);
+  
+  if(stop==1){
+      printf("\n\n0\n");
+    }else if(stop==2){
+      printf("0\n0\n1\n");
+    }else{
+      printf("0\n1\n");
+    }
+    
+  printf("0\n1\n");
+  for (cont=0;cont<stop;cont++)
+   {
+      z=x+y;
+      printf("%d\n",z);
+      x=y;
+      y=z;
+    }
+  
+return 0;
+}
