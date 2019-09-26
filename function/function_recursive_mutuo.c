f882bb295abaebbf8c3dda96f0dc14bce1bfa7fe
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-26 14:43:03 -0500

    Update function_recursive_mutuo.c

--- function/function_recursive_mutuo.c
+++ function/function_recursive_mutuo.c
@@ -17,3 +17,23 @@ void funB(char num)
 {
 	funA(--num); 
 }
+
+#include <stdio.h>
+void funA(int num);
+void funB(int num);
+
+int main(void)
+{
+	funA(1); 
+	return 0;
+}
+void funA(int num)
+{
+	if (num < 20) 
+		funB(num);
+	printf("%d ,", num);
+}
+void funB(int num)
+{
+	funA(++num); 
+}
