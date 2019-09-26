a484d1ae648d19a91c8491fe6dc63b93bd3c1af8
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-26 14:01:22 -0500

    Update function_recursive.c

--- function/function_recursive.c
+++ function/function_recursive.c
@@ -15,3 +15,29 @@ int main(void)
 
 	return 0;
 }
+
+#include <stdio.h>
+
+long factorial(int n);
+
+
+int main(void)
+{
+	/* en este caso se llama a la función y se imprime directamente*/
+	printf("%ld ", factorial(5));
+
+	return 0;
+}
+
+long factorial(int n)
+{
+	if (n == 0){ /* caso base */
+		printf("IF, N vale: 1\n ");
+		return 1; /* como 0! = 1, se retorna 1*/
+	}
+	else{
+		printf("ELSE, vale: %d\n ", n);
+		return n * factorial (n - 1); /* llamada a esta misma función */
+	}
+}
+
