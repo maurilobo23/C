5785173cd861d5c882642e9054eaf59b8b6e1a9e
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-26 14:34:01 -0500

    Update function_recursive.c

--- function/function_recursive.c
+++ function/function_recursive.c
@@ -41,3 +41,71 @@ long factorial(int n)
 	}
 }
 
+#include <stdio.h>
+
+long factorial(int n);
+
+int n;
+char stop;
+
+int main(void)
+{
+	do{/* en este caso se llama a la función y se imprime directamente*/
+	    
+	    printf("dame un numero:");
+	    
+	    scanf("%d", &n);
+	    printf("El resultado del factodial de %d, es:%1d ",n, factorial(n));
+	    printf("\nDesear salir: s/n\n");
+	    getchar();
+	   // scanf("%C", &stop);
+	}while((stop =getchar())!='s');
+    return 0;
+}
+
+long factorial(int n)
+{
+	if (n == 0){ /* caso base */
+		printf("IF, N vale: 1\n ");
+		return 1; /* como 0! = 1, se retorna 1*/
+	}
+	else{
+		printf("ELSE, vale: %d\n ", n);
+		return n * factorial (n - 1); /* llamada a esta misma función */
+	}
+}
+
+#include <stdio.h>
+
+long factorial(int n); //existir una funcion que se va a llamar factorial y como parametro recibira un entero
+int n;
+long res;
+char stop;
+
+int main(void)
+{
+	do{/* en este caso se llama a la función y se imprime directamente*/
+	    
+	    printf("dame un numero:");
+	    res = factorial (n); 
+	    scanf("%d", &n);
+	    printf("El resultado del factorial de %d, es:%1d ",n, res);
+	    printf("\nDesear salir: s/n\n");
+	    getchar();
+	   // scanf("%C", &stop);
+	}while((stop =getchar())!='s');
+    return 0;
+}
+
+long factorial(int n)
+{
+	if (n == 0){ /* caso base */
+		printf("IF, N vale: 1\n ");
+		return 1; /* como 0! = 1, se retorna 1*/
+	}
+	else{
+		printf("ELSE, vale: %d\n ", n);
+		return n * factorial (n - 1); /* llamada a esta misma función */
+	}
+}
+
