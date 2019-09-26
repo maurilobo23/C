ba9073ee68107800c17c8f0d9542971895ee24a2
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-26 13:38:13 -0500

    Update function.c

--- function/function.c
+++ function/function.c
@@ -11,3 +11,49 @@ int main(void) /* Función principal del programa */
 	saludo(); /* llamada a la función saludo*/
 	return 0; /* sale del programa con código 0 (correcto) */
 }
+
+#include <stdio.h>
+
+int main(void) /* Función principal del programa, encargada de ejecturar todo en el programa y en esas funciones puede ejecutar más */
+{
+    int x;
+ 	for(x=1; x<=50; x++){
+ 	    printf("%d :", x);
+ 	    saludo();
+    }
+    printf("fuera del for: ");
+    saludo();
+    return 0;
+}
+void saludo(void)
+{
+ 	printf("Hola Mundo\n"); /* imprime la cadena */
+	return; /* sale de la función */
+}
+
+#include <stdio.h>
+
+void saludo(void);
+int  y = 10;
+
+// funciones globales se usan en todo el programa, las locales en el bloque de funcion
+int main(void) /* Función principal del programa, encargada de ejecturar todo en el programa y en esas funciones puede ejecutar más */
+{
+    int x;
+ 	for(x=1; x<=50; x++){
+ 	    printf("%d :", x);
+ 	    //printf("la variable y vale: %d\n", y);
+ 	    saludo();
+    }
+    printf("fuera del for: ");
+    saludo();
+    return 0;
+}
+void saludo(void)
+{
+ 	printf("Hola Mundo\n"); /* imprime la cadena */
+	return; /* sale de la función */
+}
+    
+
+    
