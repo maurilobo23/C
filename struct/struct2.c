936bdb439af72a16f42ea26e88747c3424b95cf4
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-30 13:39:54 -0500

    Update struct2.c

--- struct/struct2.c
+++ struct/struct2.c
@@ -13,3 +13,19 @@ int main()
  printf("Registro: %d %s %s\n",libro.code,libro.titulo,libro.autor);
  return 0;
 } 
+
+#include <stdio.h> // Archivo de cabecera para subrutinas de e/s
+#include <stdlib.h> // Archivo de cabecera para subrutinas de utilidades
+#include "data.h" // Archivo de cabecera para subrutinas de utilidades
+// esté data lo tenemos en los archivos y por ende lo mandamos a llamar//
+int main()
+{ 
+ printf("Ingrese el codigo:");
+ scanf("%d",&libro.code);
+ printf("Ingrese el título del libro:");
+ scanf("%s",libro.titulo);
+ printf("Ingrese el nombre del autor:");
+ scanf("%s",libro.autor);
+ printf("Registro: %d %s %s\n",libro.code,libro.titulo,libro.autor);
+ return 0;
+} 
