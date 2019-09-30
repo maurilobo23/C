437705f192e0d8493b601ea0112b3da27e7ead50
Eduardo23 <54286984+maurilobo23@users.noreply.github.com>
2019-09-30 13:34:42 -0500

    Update struct.c

--- struct/struct.c
+++ struct/struct.c
@@ -23,3 +23,29 @@ printf("Ingrese el codigo:");
  printf("Registro: %d %s %s\n",libro.codigo,libro.titulo,libro.autor);
  return 0;
 } // Fin de programa| 
+
+#include <stdio.h> // Archivo de cabecera para subrutinas de e/s
+#include <stdlib.h> // Archivo de cabecera para subrutinas de utilidades
+
+struct biblioteca { // Crea la estructura del registro
+	int codigo;
+	char titulo[20]; // se genera una estructura de datos a nuestra conveniencia 
+	char autor[20];
+}libro;
+
+//struct biblioteca libro = {0}; // Define libro con el tipo de dato elibro
+
+int main()
+{ // Inicio
+ system("clear"); // Limpia pantalla
+ // Ingresa datos del registro
+printf("Ingrese el codigo:");
+ scanf("%d",&libro.codigo); // "&" apuntando una dirección de memoria
+ printf("Ingrese el título del libro:");
+ scanf("%s",libro.titulo);
+ printf("Ingrese el nombre del autor:");
+ scanf("%s",libro.autor);
+ // Imprime el registro
+ printf("Registro: %d %s %s\n",libro.codigo,libro.titulo,libro.autor);
+ return 0;
+} // Fin de programa| 
